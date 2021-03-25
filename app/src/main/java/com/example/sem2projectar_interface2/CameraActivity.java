d75e678247b4ebf5b5ae23e75835ceb983ab167e
MADPROJECT-ar <madprojectar@gmail.com>
2021-03-26 01:28:45 +0530

    Added multiple AR models to scene

--- app/src/main/java/com/example/sem2projectar_interface2/CameraActivity.java
+++ app/src/main/java/com/example/sem2projectar_interface2/CameraActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.hardware.Camera;
+import android.widget.ImageView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.ar.core.Anchor;
@@ -18,29 +19,148 @@ import com.google.ar.sceneform.ux.TransformableNode;
 
 public class CameraActivity extends AppCompatActivity {
 
-    FloatingActionButton add;
-    private ArFragment arFragment;
+    public ArFragment arFragment;
+    ImageView img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12,img13,img14;
+    final String t1 = "bedwithsidetables.sfb";
+    final String t2 = "armrestchair.sfb";
+    final String t3 = "blackcouch.sfb";
+    final String t4 = "bookshelf.sfb";
+    final String t5 = "couchwithlegrest.sfb";
+    final String t6 = "drums.sfb";
+    final String t7 = "blackpiano.sfb";
+    final String t8 = "bed.sfb";
+    final String t9 = "officedesk.sfb";
+    final String t10 = "redcouch.sfb";
+    final String t11 = "studytable.sfb";
+    final String t12 = "vintagecornertable.sfb";
+    final String t13 = "woodenchair.sfb";
+    final String t14 = "woodenpiano.sfb";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        arFragment = (ArFragment)getSupportFragmentManager().findFragmentById(R.id.arFragment);
 
         Intent i = getIntent();
         String t = i.getStringExtra(BrowseModels.EXTRA_TEXT);
 
-        add = findViewById(R.id.floatingActionButton);
-
-        add.setOnClickListener(new View.OnClickListener() {
+        img1 = findViewById(R.id.img1);
+        img1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(CameraActivity.this, BrowseModels.class);
-                startActivity(i);
+            public void onClick(View view) {
+                AR(t1);
             }
         });
 
-        arFragment = (ArFragment)getSupportFragmentManager().findFragmentById(R.id.arFragment);
+        img2 = findViewById(R.id.img2);
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AR(t2);
+            }
+        });
 
+        img3 = findViewById(R.id.img3);
+        img3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AR(t3);
+            }
+        });
+
+        img4 = findViewById(R.id.img4);
+        img4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AR(t4);
+            }
+        });
+
+        img5 = findViewById(R.id.img5);
+        img5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AR(t5);
+            }
+        });
+
+        img6 = findViewById(R.id.img6);
+        img6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AR(t6);
+            }
+        });
+
+        img7 = findViewById(R.id.img7);
+        img7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AR(t7);
+            }
+        });
+
+        img8 = findViewById(R.id.img8);
+        img8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AR(t8);
+            }
+        });
+
+        img9 = findViewById(R.id.img9);
+        img9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AR(t9);
+            }
+        });
+
+        img10 = findViewById(R.id.img10);
+        img10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AR(t10);
+            }
+        });
+
+        img11 = findViewById(R.id.img11);
+        img11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AR(t11);
+            }
+        });
+
+        img12 = findViewById(R.id.img12);
+        img12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AR(t12);
+            }
+        });
+
+        img13 = findViewById(R.id.img13);
+        img13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AR(t13);
+            }
+        });
+
+        img14 = findViewById(R.id.img14);
+        img14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AR(t14);
+            }
+        });
+
+
+    }
+    public void AR(String t){
         arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
 
             Anchor anchor = hitResult.createAnchor();
@@ -50,7 +170,6 @@ public class CameraActivity extends AppCompatActivity {
                     .build()
                     .thenAccept(modelRenderable -> addModelToScene(anchor,modelRenderable));
         });
-
     }
     private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
 
@@ -61,7 +180,6 @@ public class CameraActivity extends AppCompatActivity {
 
         arFragment.getArSceneView().getScene().addChild(node);
         transformableNode.select();
-
-
     }
+
 }
