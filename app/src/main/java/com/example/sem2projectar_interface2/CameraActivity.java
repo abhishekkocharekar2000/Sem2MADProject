c1cea39399932176ecb11a38180bff3600f976e9
MADPROJECT-ar <madprojectar@gmail.com>
2021-03-26 12:09:17 +0530

    Video recording of AR scene

--- app/src/main/java/com/example/sem2projectar_interface2/CameraActivity.java
+++ app/src/main/java/com/example/sem2projectar_interface2/CameraActivity.java
@@ -1,15 +1,25 @@
 package com.example.sem2projectar_interface2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.CamcorderProfile;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.hardware.Camera;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.sem2projectar_interface2.ui.Screenshot;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.ar.core.Anchor;
 import com.google.ar.sceneform.AnchorNode;
@@ -17,9 +27,21 @@ import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+
+import static java.text.DateFormat.getDateInstance;
+
 public class CameraActivity extends AppCompatActivity {
 
     public ArFragment arFragment;
+    private Screenshot s;
+    HorizontalScrollView h;
+    FloatingActionButton f;
     ImageView img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12,img13,img14;
     final String t1 = "bedwithsidetables.sfb";
     final String t2 = "armrestchair.sfb";
@@ -40,11 +62,36 @@ public class CameraActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        h = findViewById(R.id.horizontalScrollView);
+        f = findViewById(R.id.floatingActionButton);
 
         arFragment = (ArFragment)getSupportFragmentManager().findFragmentById(R.id.arFragment);
 
         Intent i = getIntent();
         String t = i.getStringExtra(BrowseModels.EXTRA_TEXT);
+
+        //verifyStoragePermission(this);
+        Toast.makeText(CameraActivity.this, "Started Recording", Toast.LENGTH_SHORT).show();
+
+        f.setOnClickListener(view -> {
+            if(s == null){
+                s = new Screenshot();
+                s.setSceneView(arFragment.getArSceneView());
+
+                int orientation = getResources().getConfiguration().orientation;
+
+                s.setVideoQuality(CamcorderProfile.QUALITY_HIGH, orientation);
+            }
+
+            boolean isRecording = s.onToggleRecord();
+
+            if(isRecording)
+                Toast.makeText(CameraActivity.this, "Started Recording", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(CameraActivity.this, "Stopped Recording", Toast.LENGTH_SHORT).show();
+
+
+        });
 
         img1 = findViewById(R.id.img1);
         img1.setOnClickListener(new View.OnClickListener() {
@@ -155,11 +202,24 @@ public class CameraActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 AR(t14);
+
             }
         });
 
 
     }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+
+    }
+
     public void AR(String t){
         arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
 
@@ -180,6 +240,8 @@ public class CameraActivity extends AppCompatActivity {
 
         arFragment.getArSceneView().getScene().addChild(node);
         transformableNode.select();
+
+
     }
 
 }
