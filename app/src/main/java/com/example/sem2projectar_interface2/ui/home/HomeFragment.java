4def1129d437f0f5e9323226f9467e1d9a78a9e4
Harsh Walawalkar <hwalawalkar@gmail.com>
2021-04-27 00:20:29 +0530

    Finished Carousel

--- app/src/main/java/com/example/sem2projectar_interface2/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/sem2projectar_interface2/ui/home/HomeFragment.java
@@ -4,23 +4,47 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.sem2projectar_interface2.R;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageClickListener;
+import com.synnapps.carouselview.ImageListener;
 
 public class HomeFragment extends Fragment {
+    private int[] mImage = new int[]{
+            R.drawable.chair, R.drawable.cupboard, R.drawable.greysofa, R.drawable.redsofa, R.drawable.table
+    };
+
+    private String[] mImageTitle = new String[]{
+        "Chair","Cupboard","Grey Sofa","Red Sofa","Table"
+    };
 
     private HomeViewModel homeViewModel;
 
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        CarouselView carouselView = root.findViewById(R.id.carouselView);
+        carouselView.setPageCount(mImage.length);
+        carouselView.setImageListener(new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                imageView.setImageResource(mImage[position]);
+            }
+        });
+        carouselView.setImageClickListener(new ImageClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(getContext(), mImageTitle[position], Toast.LENGTH_SHORT).show();
+            }
+        });
         return root;
     }
 }
