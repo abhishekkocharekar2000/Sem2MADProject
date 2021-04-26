c7b0364930e6cc54677db68d0e5ea521c8b09d24
Harsh Walawalkar <hwalawalkar@gmail.com>
2021-04-26 23:54:57 +0530

    added carousel frame and images

--- app/src/main/java/com/example/sem2projectar_interface2/BrowseModels.java
+++ app/src/main/java/com/example/sem2projectar_interface2/BrowseModels.java
@@ -15,7 +15,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class BrowseModels extends AppCompatActivity {
 
