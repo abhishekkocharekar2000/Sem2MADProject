b883d356281c9abfec813adf5c897f98c238645e
Harsh Walawalkar <hwalawalkar@gmail.com>
2021-04-26 23:17:40 +0530

    added carousel frame

--- app/src/main/java/com/example/sem2projectar_interface2/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/sem2projectar_interface2/ui/home/HomeFragment.java
@@ -1,12 +1,9 @@
 package com.example.sem2projectar_interface2.ui.home;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
